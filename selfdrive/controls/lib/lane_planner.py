7e6d54895b3519a035f74c5d3a7be8945e524b37
Donald Yang <51182726+donald0415@users.noreply.github.com>
2025-05-05 15:48:11 +0800

    Update lane_planner.py
    如果您發現它總是靠一側行駛，為什麼不修理它呢？您需要做的就是調整 openpilot/selfdrive/controls/lib/lane_planner.py 中的 path_offset。

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -10,6 +10,8 @@ from openpilot.common.swaglog import cloudlog
 TRAJECTORY_SIZE = 33
 # camera offset is meters from center car to camera
 # model path is in the frame of the camera
+# 如果您發現它總是靠一側行駛，為什麼不修理它呢？
+# 您需要做的就是調整 openpilot/selfdrive/controls/lib/lane_planner.py 中的 path_offset。
 PATH_OFFSET = 0.00
 CAMERA_OFFSET = 0.04
 
